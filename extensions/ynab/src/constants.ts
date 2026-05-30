155cead63054ce93380f3fb16250d450e3438f8e
Nathaniel Blackburn <support@nblackburn.uk>
2026-05-30 22:56:32 +0100

    add hook to handle authenticated requests

--- extensions/ynab/src/constants.ts
+++ extensions/ynab/src/constants.ts
@@ -6,3 +6,6 @@ export const OAUTH_TOKEN_URL =
   "https://oauth.raycast.com/v1/token/ak9nm5BJ8D9pw4-3-86VpoRjZZVBLDs8xD_6CmX0TC8bdXK8xl1MVdkTA3rC8fEl3gXgtC6iG0yeG_DYKyS_HQtDwoyYAyhZWJYaiHqCmv6ylmBIyj1G1_nwfldh4OIJm2E6RFu7dyps";
 export const OAUTH_REFRESH_TOKEN_URL =
   "https://oauth.raycast.com/v1/refresh-token/yYjE-AXfpvqLb9-xf8NAvYF5Ubr0h8C7BMt3azm9Sgmn4W2k7N8ON5t40V_HwSkhrZmGV5wbquW-ZMN4YGhGnGkIi_evpbYTPz6cHgL0qa9k0piL_KhapIT8hudr3DeA6DsWYwOBZlWa";
+
+// API
+export const API_BASE_URL = "https://api.ynab.com/v1";
---
+++ extensions/ynab/src/hooks/use-api.ts
@@ -0,0 +1,15 @@
+import { API_BASE_URL } from "~/constants";
+import { getAccessToken, RequestInfo, useFetch } from "@raycast/utils";
+
+export const useApi = <T>(url: RequestInfo, options?: RequestInit) => {
+  const { token } = getAccessToken();
+
+  return useFetch<T>(`${API_BASE_URL}/${url}`, {
+    keepPreviousData: true,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+
+    ...options,
+  });
+};
