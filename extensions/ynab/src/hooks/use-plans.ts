4040f96714a3371c8a4f764140e113642d8131a9
Nathaniel Blackburn <support@nblackburn.uk>
2026-06-01 22:59:59 +0100

    add hook for requesting plan data

---
+++ extensions/ynab/src/hooks/use-plans.ts
@@ -0,0 +1,17 @@
+import { useMemo } from "react";
+import { useApi } from "./use-api";
+import { PlansResponse } from "~/types";
+
+export const usePlans = () => {
+  const { isLoading, data } = useApi<PlansResponse>("plans");
+
+  const plans = useMemo(() => {
+    return data?.data?.plans;
+  }, [data]);
+
+  const defaultPlan = useMemo(() => {
+    return data?.data?.default_plan;
+  }, [data]);
+
+  return { isLoading, plans, defaultPlan };
+};
