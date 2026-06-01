fa46a607103d2e48b5b3442e903f0f4de21b2489
Nathaniel Blackburn <support@nblackburn.uk>
2026-06-01 22:42:35 +0100

    add plan types

--- extensions/ynab/src/types.ts
+++ extensions/ynab/src/types.ts
@@ -60,3 +60,35 @@ export type CategoriesResponse = {
 };
 
 export type PlanOptions = "default" | "last-used" | (string & {});
+
+export type DateFormat = {
+  format: string;
+};
+
+export type CurrencyFormat = {
+  iso_code: string;
+  example_format: string;
+  decimal_digits: number;
+  decimal_separator: string;
+  symbol_first: boolean;
+  group_separator: string;
+  currency_symbol: string;
+  display_symbol: boolean;
+};
+
+export type Plan = {
+  id: string;
+  name: string;
+  last_modified_on: string;
+  first_month: string;
+  last_month: string;
+  date_format: DateFormat;
+  currency_format: CurrencyFormat;
+};
+
+export type PlansResponse = {
+  data: {
+    plans: Plan[];
+    default_plan: Plan;
+  };
+};
