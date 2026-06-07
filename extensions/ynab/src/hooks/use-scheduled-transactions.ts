01e183e3bd701bb0472437adbf19241313d8a75e
Nathaniel Blackburn <support@nblackburn.uk>
2026-06-07 16:04:41 +0100

    add hook for requesting scheduled transactions

---
+++ extensions/ynab/src/hooks/use-scheduled-transactions.ts
@@ -0,0 +1,23 @@
+import { useCallback, useMemo } from "react";
+import { useApi } from "./use-api";
+import { PlanOptions, ScheduledTransactionsResponse } from "~/types";
+
+export const useScheduledTransactions = (plan: PlanOptions = "default") => {
+  const { isLoading, data } = useApi<ScheduledTransactionsResponse>(
+    `plans/${plan}/scheduled_transactions`,
+  );
+
+  const scheduledTransactions = useMemo(() => {
+    return data?.data?.scheduled_transactions;
+  }, [data]);
+
+  const getScheduledForCategory = useCallback(
+    (categoryId: string) =>
+      scheduledTransactions?.filter(
+        (scheduleTransaction) => scheduleTransaction.category_id === categoryId,
+      ),
+    [scheduledTransactions],
+  );
+
+  return { isLoading, scheduledTransactions, getScheduledForCategory };
+};
